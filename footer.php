392b9c20bb720a3a55c5d3ef89074f9bcba81ef7
Justin <ju3tin@hotmail.co.uk>
2021-02-13 17:16:25 +0000

    github

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
-		<p>jQuery Mobile Demos version <span class="jqm-version"></span></p>
+		<p>Imperial Token <!--<span class="jqm-version">--></span></p>
 		<p>Copyright 2014 Imperial Token</p>
 	</div>
