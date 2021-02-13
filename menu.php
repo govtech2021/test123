7d123b38ecfd0125650ac897fc9f47ba4efb834d
Justin <ju3tin@hotmail.co.uk>
2021-02-13 18:04:47 +0000

    update

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
 	    	<ul class="jqm-list ui-alt-icon ui-nodisc-icon">
 <li data-filtertext="demos homepage" data-icon="home"><a href=".././">Home</a></li>
-<li data-filtertext="introduction overview getting started"><a href="../intro/" data-ajax="false">Register/Login</a></li>
+<li data-filtertext="introduction overview getting started"><a href="../register.php" data-ajax="false">Register/Login</a></li>
 <li data-filtertext="buttons button markup buttonmarkup method anchor link button element"><a href="../button-markup/" data-ajax="false">Assisant</a></li>
 <li data-filtertext="form button widget input button submit reset"><a href="../button/" data-ajax="false">Button widget</a></li>
 
