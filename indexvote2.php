aaf83a7b9ef4e62598221ffcc4d8ab261bc40b31
Justin <ju3tin@hotmail.co.uk>
2021-02-14 10:31:35 +0000

    ipdate

---
+++ indexvote2.php
@@ -0,0 +1,14 @@
+
+    <div role="main" class="ui-content jqm-content">
+    <fieldset data-role="controlgroup">
+    <legend>Radio buttons, vertical controlgroup:</legend>
+        <input type="radio" name="radio-choice-1" id="radio-choice-1" value="choice-1" checked="checked">
+        <label for="radio-choice-1">Canditate 1</label>
+        <input type="radio" name="radio-choice-1" id="radio-choice-2" value="choice-2">
+        <label for="radio-choice-2">Canditate 2</label>
+        <input type="radio" name="radio-choice-1" id="radio-choice-3" value="choice-3">
+        <label for="radio-choice-3">Conditate 3</label>
+        <input type="radio" name="radio-choice-1" id="radio-choice-4" value="choice-4">
+        <label for="radio-choice-4">Canditate 4</label>
+</fieldset>
+</div>
