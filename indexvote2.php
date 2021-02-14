63f3173f7ff7a1c2a9992a4523bcd99c629e85f7
Justin <ju3tin@hotmail.co.uk>
2021-02-14 11:31:34 +0000

    update

--- indexvote2.php
+++ indexvote2.php
@@ -1,15 +1,15 @@
 
     <div role="main" class="ui-content jqm-content">
     <fieldset data-role="controlgroup">
-    <legend>Please chose your canditate:</legend>
+    <legend>Please chose your Candidate:</legend>
         <input type="radio" name="radio-choice-1" id="radio-choice-1" value="choice-1" checked="checked">
-        <label for="radio-choice-1">Canditate 1</label>
+        <label for="radio-choice-1">Candidate 1st</label>
         <input type="radio" name="radio-choice-1" id="radio-choice-2" value="choice-2">
-        <label for="radio-choice-2">Canditate 2</label>
+        <label for="radio-choice-2">Candidate 2nd</label>
         <input type="radio" name="radio-choice-1" id="radio-choice-3" value="choice-3">
-        <label for="radio-choice-3">Conditate 3</label>
+        <label for="radio-choice-3">Candidate 3rd</label>
         <input type="radio" name="radio-choice-1" id="radio-choice-4" value="choice-4">
-        <label for="radio-choice-4">Canditate 4</label>
+        <label for="radio-choice-4">Candidate 4th</label>
 </fieldset>
 <a href="/thankyou.php" class="ui-shadow ui-btn ui-corner-all">Set Vote</a>
     
