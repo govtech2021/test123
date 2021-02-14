b3732486d4803f559ffd13883026c4be96f47cc4
Justin <ju3tin@hotmail.co.uk>
2021-02-14 01:38:25 +0000

    update

--- indexassist.php
+++ indexassist.php
@@ -1,13 +1,43 @@
-<script>
-  window.watsonAssistantChatOptions = {
-      integrationID: "3a2e7fe3-622f-4eba-a3d0-deffbe9c2a99", // The ID of this integration.
-      region: "eu-gb", // The region your integration is hosted in.
-      serviceInstanceID: "cdaa8ff3-b6fb-4ac3-920b-61df16e256f4", // The ID of your service instance.
-      onLoad: function(instance) { instance.render(); }
-    };
-  setTimeout(function(){
-    const t=document.createElement('script');
-    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
-    document.head.appendChild(t);
-  });
-</script>
+
+    <div role="main" class="ui-content jqm-content">
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Hello there, how can I help you?</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Type something here.." required>
+                <button id="send-btn">Send</button>
+            </div></div>
+      
+    
+
+    <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+
+                // start ajax code
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text='+$value,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
--- testassist.php
+++ testassist.php
@@ -21,10 +21,10 @@
 		<a href="#" class="jqm-navmenu-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-bars ui-nodisc-icon ui-alt-icon ui-btn-left">Menu</a>
 		<a href="#" class="jqm-search-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-search ui-nodisc-icon ui-alt-icon ui-btn-right">Search</a>
 	</div><!-- /header -->
-<?php include 'indexassist.php';?>
+
     <?php include 'menu.php';?><!-- /panel -->
 
-
+	<?php include 'indexassist.php';?>
 	<?php include 'footer.php';?><!-- /footer -->
 	<!-- TODO: This should become an external panel so we can add input to markup (unique ID) -->
     <div data-role="panel" class="jqm-search-panel" data-position="right" data-display="overlay" data-theme="a">
