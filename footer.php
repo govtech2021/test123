557cc79c32135b160fe67508570af7560317d19b
Justin <ju3tin@hotmail.co.uk>
2021-02-15 15:57:00 +0000

    update

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer" style="background-color: transparent !important;">
+
+
+<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer" style="background-color: transparent !important;border-color: transparent !important;">
 		<p>Imperial Token <!--<span class="jqm-version">--></span></p>
 		<p>Copyright 2014 Imperial Token</p>
 	</div>
