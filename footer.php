30ebd46fb75dc714122fca5d73fa8a421533fc86
Justin <ju3tin@hotmail.co.uk>
2021-02-15 15:43:59 +0000

    update

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
+<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer" style="background-color: transparent !important;">
 		<p>Imperial Token <!--<span class="jqm-version">--></span></p>
 		<p>Copyright 2014 Imperial Token</p>
 	</div>
---
+++ pageheader.php
@@ -0,0 +1,10 @@
+<div data-role="page" class="jqm-demos jqm-home">
+
+	<div data-role="header" class="jqm-header">
+		<h2><img src="_assets/img/logo1.png" alt="jQuery Mobile"></h2>
+		<p>Version <span class="jqm-version"></span></p>
+		<a href="#" class="jqm-navmenu-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-bars ui-nodisc-icon ui-alt-icon ui-btn-left">Menu</a>
+		<a href="#" class="jqm-search-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-mic ui-nodisc-icon ui-alt-icon ui-btn-right" style="
+    background-image: url('black-logocrop.png');
+">Search</a>
+</div>
